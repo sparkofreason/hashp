35626686c105be2f4cd9d88ada3767908ffe9cbf
Dave Dixon <dave.dixon@creditkarma.com>
2020-01-02 10:09:57 -0800

    Fix tap> example

--- examples/hasht.clj
+++ examples/hasht.clj
@@ -2,15 +2,14 @@
   (:require [hashtag.core :as ht :refer [defhashtag]]
             [clojure.pprint :refer [pprint]]))
 
-(defhashtag t/foo #(tap> (assoc % :tag :foo)))
-(defhashtag t/bar #(tap> (assoc % :tag :bar)))
+(defhashtag t tap>)
 
 (defn mean [xs]
-  (/ (double #t/foo (reduce + xs)) #t/bar (count xs)))
+  (/ (double #t ^{:t :foo} (reduce + xs)) #t ^{:t :bar} (count xs)))
 
 (defn tap-fn
   [x]
-  (case (:tag x)
+  (case (get-in x [:metadata :t])
     :foo (println "FOOOOOOOOOO!")
     :bar (println "BAAAAAAAAAR!"))
   (pprint (select-keys x [:form :result])))
